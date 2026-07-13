f299b3b423148933ac965dd5426e4f9c908324f3
dunong03 <isaacy03@gmail.com>
2026-07-13 12:36:05 +0900

    initial commit

---
+++ src/test/java/com/example/jobpuzzle/JobpuzzleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobpuzzle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobpuzzleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
