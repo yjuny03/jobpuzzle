b8390d180da53a635f31a8cd6818d48677ed59f9
seolgi <seolgi100k@gmail.com>
2026-07-17 12:14:29 +0900

    FinalReport 설계에 맞게 엔티티 수정 및 JSON 구조 잡아서 MOCK 데이터 넣음

--- src/main/java/com/example/jobpuzzle/ai/service/AiClientService.java
+++ src/main/java/com/example/jobpuzzle/ai/service/AiClientService.java
@@ -1,16 +1,12 @@
 package com.example.jobpuzzle.ai.service;
 
 import com.example.jobpuzzle.ai.client.AiClient;
-import com.example.jobpuzzle.ai.client.MockAiClient;
 import com.example.jobpuzzle.ai.dto.CandidateMaterialAnalysisResult;
+import com.example.jobpuzzle.ai.dto.FinalReportResult;
 import com.example.jobpuzzle.ai.dto.JobPostingAnalysisResult;
 import com.example.jobpuzzle.ai.dto.QuestionGenerationResult;
-import com.example.jobpuzzle.analysis.entity.JobPostingAnalysis;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 //@RequiredArgsConstructor
@@ -36,6 +32,8 @@ public class AiClientService {
     public QuestionGenerationResult generateQuestions(String prompt) {
         return aiClient.generateQuestions(prompt);
     }
+
+    public FinalReportResult finalReport(String prompt) { return aiClient.finalReport(prompt);}
 
     public void callAi() {
         // TODO: 클래스 정의서 기준으로 구현
